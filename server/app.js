f79e6e7acd3c8eb5f633407641e7725c2f93debf
马昭岑 <mazhaocen@ky-tech.com.cn>
2019-07-15 17:18:52 +0800

    333

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('koa-bodyparser')
 const cors = require('koa2-cors')
 const mongoose = require('mongoose');
 const app = new Koa()
+const JwtUtil = require('./token/token');
 
 app.use(bodyParser())
 
@@ -18,18 +19,50 @@ db.once('open',(cb) => {
 app.use(cors({
   origin: (ctx) => {
     ctx.set("Access-Control-Allow-Origin", "http://localhost:8082");
-    ctx.set("Access-Control-Allow-Headers",'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild')
-    ctx.set("Access-Control-Allow-Credentials",true) // 如果 要带参数 或 cookie   withCredentials: true 要设置为 true
+    ctx.set("Access-Control-Allow-Headers",'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild, Token, Key, User',)
+    ctx.set("Access-Control-Allow-Credentials", true) // 如果 要带参数 或 cookie   withCredentials: true 要设置为 true
+    ctx.set("Access-Control-Expose-Headers", 'Token') // 如果 要带参数 或 cookie   withCredentials: true 要设置为 true
   },
   exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
   maxAge: 5,
   credentials: true,
   allowMethods: ['OPTIONS', 'GET', 'POST', 'PUT', 'DELETE'],
-  allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept', 'Token'],
 }));
 
-const router = require('./router')
+let jwt = new JwtUtil()
+app.use(async (ctx, next) => {
+  // console.log(ctx.request.path, 1111111111);
+  if (ctx.request.path === '/users/login'){
 
+   await next()
+  } else {
+    let token = ctx.request.headers.token
+    let res = await jwt.verifyToken(token)
+    // console.log(res, 2222222222);
+    if (!res) {
+      ctx.response.body = {
+        code: 0,
+        msg: '登录过期'
+      }
+    } else {
+      console.log(res.exp - Math.floor(Date.now() / 1000));
+      if ((res.exp - Math.floor(Date.now()/1000)) < 300) {
+        console.log('token 快过期了');
+        let data = {
+          userName:res.userName,
+          id: res.id
+        }
+        // let jwt = new JwtUtil(data)
+        token = jwt.createToken(data)
+      }
+      ctx.set('Token', token)
+      await next()
+    }
+  }
+})
+
+const router = require('./router')
 app.use(router().routes())
 app.use(router().allowedMethods());
 app.listen(3000)
--- server/controllers/demo.js
+++ server/controllers/demo.js
@@ -1,21 +1,13 @@
 
-let demo = async (ctx, next) => {
-  ctx.response.body = {
-    code:1,
-    data:'成功',
-    msg:'请求成功'
-  }
-}
-
-module.exports = {
-  'POST-/users/demo': demo,
-}
-// signIn () {
-//   console.log(123);
-//   this.$http.post('/users/signIn', {
-//     userName: '12332',
-//     password: 222232
-//   }).then(res => {
-//     console.log(res);
-//   })
+// let demo = async (ctx, next) => {
+//   ctx.response.body = {
+//     code:1,
+//     data:'成功',
+//     msg:'请求成功'
+//   }
 // }
+//
+// module.exports = {
+//   'POST-users/demo': demo,
+// }
+
